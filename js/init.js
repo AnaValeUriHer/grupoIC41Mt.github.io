b5938fc04222dfc698c02ecb5d56fc95df5a69ed
AnaValeUriHer <88065980+AnaValeUriHer@users.noreply.github.com>
2021-07-27 20:17:54 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyCRLV2OxlIPJ21syWGwZcS2Am_EimSElW0",
+    authDomain: "auten41m.firebaseapp.com",
+    projectId: "auten41m",
+    storageBucket: "auten41m.appspot.com",
+    messagingSenderId: "818321749551",
+    appId: "1:818321749551:web:35292f7dbbf12dfa9bf8da",
+    measurementId: "G-849H7ZKY0X"
+});
